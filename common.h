ac7f96c6b2b84ccadecd18ba102a461957da7f3e
azrdev <azrdev@qrdn.de>
2018-01-16 16:26:09 +0100

    implement antlr conversion, add main() utilizing it

--- common.h
+++ common.h
@@ -1,1 +1,34 @@
 #pragma once
+
+#include <unordered_map>
+#include <algorithm>
+
+
+#define C2A_TODO "TODO (coco_to_antlr): "
+
+template<class Key, class Value>
+class UBiMap {
+private:
+    std::unordered_map<Key, Value> forward;
+    std::unordered_map<Value, Key> backward;
+
+public:
+    void insert(Key key, Value value) {
+        forward.insert({key, value});
+        backward.insert({value, key});
+    }
+
+    bool contains_key(const Key& key) {
+        return forward.find(key) != forward.end();
+    }
+
+    bool contains_value(const Value& value) {
+        return backward.find(value) != backward.end();
+    }
+
+    Value& at(const Key& key) {
+        return forward.at(key);
+    }
+};
+
+enum class name_type { charset, token, production };
--- main.cpp
+++ main.cpp
@@ -1,3 +1,48 @@
-int main() {
-	return 0;
+#include <vector>
+#include <fstream>
+#include <iostream>
+#include "Parser.h"
+
+
+//! wrapper around coco_string_* functions
+class coco_wstring_t {
+public:
+    explicit coco_wstring_t(const char* value) :
+            data(coco_string_create(value))
+    {};
+    ~coco_wstring_t() {
+        coco_string_delete(data);
+    }
+    wchar_t* raw() { return data; }
+private:
+    wchar_t* data;
+};
+
+
+void usage() {
+    std::wcout << L"compiled " __DATE__ << " " << __TIME__
+               << L"\nUsage: coco_to_antlr Coco.atg [ antlr.g4 ]\n\n"
+               << L"\tConvert the Coco/R grammar Coco.atg to an ANTLR4 grammar, written to antlr.g4 or standard output.\n";
+}
+
+
+int main(int argc, char* argv[]) {
+    using namespace coco_to_antlr;
+    const std::vector<std::string> arguments(argv, argv + argc);
+    if (arguments.size() < 2) {
+        std::wcerr << L"not enough arguments!\n";
+        usage();
+        return 1;
+    }
+
+    coco_wstring_t input_name(arguments.at(1).c_str());
+    auto scanner = std::make_unique<Scanner>(input_name.raw());
+    auto parser = std::make_unique<Parser>(scanner.get());
+    std::wofstream output_file;
+    if (arguments.size() >= 3) {
+        output_file.open(arguments.at(2));
+        parser->output_stream = output_file;
+    }
+    parser->Parse();
+    return 0;
 }
