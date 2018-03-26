c5e41d778eb9410f795010720f8a440cd15a1371
azrdev <azrdev@qrdn.de>
2018-03-26 22:03:21 +0200

    implement --no-instrumentation

--- main.cpp
+++ main.cpp
@@ -34,37 +34,46 @@ private:
 
 void usage() {
     std::wcout << L"compiled " __DATE__ << " " << __TIME__
-               << L"\nUsage: coco_to_antlr Coco.atg [ antlr.g4 ]\n\n"
-               << L"\tConvert the Coco/R grammar Coco.atg to an ANTLR4 grammar, written to antlr.g4 or standard output.\n";
+               << L"\nUsage: coco_to_antlr [ --no-instrumentation ] Coco.atg [ antlr.g4 ]\n\n"
+               << L"\tConvert the Coco/R grammar Coco.atg to an ANTLR4 grammar, written to antlr.g4 or standard output.\n"
+               << L"\tIf --no-instrumentation is given, don't output converted (. SemText .) and rule <attributes>.\n";
 }
 
 
-void do_parse(coco_wstring_t& input, std::reference_wrapper<std::wostream> output) {
+void do_parse(coco_wstring_t& input,
+              std::reference_wrapper<std::wostream> output,
+              bool convert_instrumentation) {
     using namespace coco_to_antlr;
     auto scanner = std::make_unique<Scanner>(input.raw());
     auto parser = std::make_unique<Parser>(scanner.get());
     parser->output_stream = output;
+    parser->convert_instrumentation = convert_instrumentation;
     parser->Parse();
 }
 
 
 int main(int argc, char* argv[]) {
-    const std::vector<std::string> arguments(argv, argv + argc);
+    std::vector<std::string> arguments(argv, argv + argc);
+    bool convert_instrumentation = true;
     if (arguments.size() < 2) {
         std::wcerr << L"not enough arguments!\n";
         usage();
         return 1;
     }
 
+    if(arguments.at(1) == "--no-instrumentation") {
+        arguments.erase(arguments.begin() +1);
+        convert_instrumentation = true;
+    }
     coco_wstring_t input_name(arguments.at(1).c_str());
     if (arguments.size() >= 3) {
         std::locale locale(std::locale::classic(), new std::codecvt_utf8<wchar_t>); // necessary to output non-ASCII, see <https://stackoverflow.com/a/3950795>
         std::wofstream output_file(arguments.at(2));
         output_file.imbue(locale);
         //TODO: input encoding
-        do_parse(input_name, output_file);
+        do_parse(input_name, output_file, convert_instrumentation);
     } else {
-        do_parse(input_name, std::ref(std::wcout));
+        do_parse(input_name, std::ref(std::wcout), convert_instrumentation);
     }
     return 0;
 }
